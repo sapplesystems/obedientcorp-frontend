733063fa3d5a61f3765727e9bbc03b146eb129cc
= <=>
2020-02-27 18:58:44 +0530

    Project, sub project and plot work :- 27-Feb-2020

---
+++ demo_2/add-plot.php
@@ -0,0 +1,70 @@
+<?php include_once 'header.php'; ?>
+<!-- partial -->
+<div class="main-panel">
+  <div class="content-wrapper ">
+    <div class="row grid-margin">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body p-3">
+            <h4 class="card-title mb-4">Add Plot</h4>
+            <div id="error_html"></div>
+            <form class="forms-sample" method="post" action="" id="plot-form">
+              <div class="row">
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label>Project Name</label>
+                    <select class="form-control" id="projects" name="projects">
+                      <option>--Select--</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-3" style="display:none;" id="sub_pro_div">
+                  <div class="form-group">
+                    <label>Sub Project Name</label>
+                    <select class="form-control" id="sub_projects" name="sub_projects">
+                      s
+                    </select>
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label>Plot No.</label>
+                    <input type="text" class="form-control" placeholder="Plot No"id="plot_no" name="plot_no">
+                  </div>
+                </div>
+                <div class="col-sm-3">
+                  <div class="form-group">
+                    <label>Plot Area</label>
+                    <input type="text" class="form-control" placeholder="Plot Area" id="plot_area" name="plot_area">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-12">
+                  <button type="submit" class="btn btn-gradient-primary mr-2">Submit</button>
+                  <input type="hidden" id="plot_id" />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-body p-3">
+            <h4 class="card-title mb-4">Edit/View Plot</h4>
+            <div class="overflowAuto">
+              <table class="table table-bordered custom_action plot_list" id="order-listing">
+
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- content-wrapper ends -->
+  <?php include_once 'footer.php'; ?>
+  <script src="../assets/javascript/plot.js"></script>
--- demo_2/header.php
+++ demo_2/header.php
@@ -5,11 +5,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Purple Admin</title>
+    <title>Obedient</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="../assets/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
     <!-- endinject -->
+	<!-- Plugin css for this page -->
+    <link rel="stylesheet" href="../assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css">
     <!-- Plugin css for this page -->
     <link rel="stylesheet" href="../assets/vendors/font-awesome/css/font-awesome.min.css" />
     <link rel="stylesheet" href="../assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css">
@@ -27,13 +29,13 @@
         <!-- partial:partials/_navbar.html -->
         <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
             <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-                <a class="navbar-brand brand-logo" href="login.php"><img src="../assets/images/logo.svg" alt="logo" /></a>
-                <a class="navbar-brand brand-logo-mini" href="login.php"><img src="../assets/images/logo-mini.svg" alt="logo" /></a>
+                <a class="navbar-brand brand-logo" href="login.php"><img src="../assets/images/logo.png" alt="logo" /></a>
+                <a class="navbar-brand brand-logo-mini" href="login.php"><img src="../assets/images/logo.png" alt="logo" /></a>
             </div>
             <div class="navbar-menu-wrapper d-flex align-items-stretch">
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
-                    <span class="mdi mdi-menu"></span>
-                </button>
+            <span class="mdi mdi-menu"></span>
+          </button>
                 <div class="search-field d-none d-md-block">
                     <form class="d-flex align-items-center h-100" action="#">
                         <div class="input-group">
@@ -44,7 +46,7 @@
                         </div>
                     </form>
                 </div>
-                <ul class="navbar-nav navbar-nav-right">
+                 <ul class="navbar-nav navbar-nav-right">
                     <li class="nav-item nav-profile dropdown">
                         <a class="nav-link dropdown-toggle" id="profileDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
                             <div class="nav-profile-img">
@@ -52,124 +54,26 @@
                                 <span class="availability-status online"></span>
                             </div>
                             <div class="nav-profile-text">
-                                <p class="mb-1" id="user_login"></p>
+                                <p class="mb-1">David Greymaax</p>
                             </div>
                         </a>
                         <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
                             <a class="dropdown-item" href="#">
                                 <i class="mdi mdi-cached mr-2 text-success"></i> Activity Log </a>
                             <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">
+                            <a class="dropdown-item" href="login.php">
                                 <i class="mdi mdi-logout mr-2 text-primary"></i> Signout </a>
                         </div>
                     </li>
-                    <li class="nav-item d-none d-lg-block full-screen-link">
-                        <a class="nav-link">
-                            <i class="mdi mdi-fullscreen" id="fullscreen-button"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link count-indicator dropdown-toggle" id="messageDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
-                            <i class="mdi mdi-email-outline"></i>
-                            <span class="count-symbol bg-warning"></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
-                            <h6 class="p-3 mb-0">Messages</h6>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img src="../assets/images/faces/face4.jpg" alt="image" class="profile-pic">
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a message</h6>
-                                    <p class="text-gray mb-0"> 1 Minutes ago </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img src="../assets/images/faces/face2.jpg" alt="image" class="profile-pic">
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a message</h6>
-                                    <p class="text-gray mb-0"> 15 Minutes ago </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <img src="../assets/images/faces/face3.jpg" alt="image" class="profile-pic">
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject ellipsis mb-1 font-weight-normal">Profile picture updated</h6>
-                                    <p class="text-gray mb-0"> 18 Minutes ago </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <h6 class="p-3 mb-0 text-center">4 new messages</h6>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-                            <i class="mdi mdi-bell-outline"></i>
-                            <span class="count-symbol bg-danger"></span>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-                            <h6 class="p-3 mb-0">Notifications</h6>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <div class="preview-icon bg-success">
-                                        <i class="mdi mdi-calendar"></i>
-                                    </div>
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject font-weight-normal mb-1">Event today</h6>
-                                    <p class="text-gray ellipsis mb-0"> Just a reminder that you have an event today </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <div class="preview-icon bg-warning">
-                                        <i class="mdi mdi-settings"></i>
-                                    </div>
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject font-weight-normal mb-1">Settings</h6>
-                                    <p class="text-gray ellipsis mb-0"> Update dashboard </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item preview-item">
-                                <div class="preview-thumbnail">
-                                    <div class="preview-icon bg-info">
-                                        <i class="mdi mdi-link-variant"></i>
-                                    </div>
-                                </div>
-                                <div class="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                                    <h6 class="preview-subject font-weight-normal mb-1">Launch Admin</h6>
-                                    <p class="text-gray ellipsis mb-0"> New admin wow! </p>
-                                </div>
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <h6 class="p-3 mb-0 text-center">See all notifications</h6>
-                        </div>
-                    </li>
                     <li class="nav-item nav-logout d-none d-lg-block">
-                        <a class="nav-link" href="#">
+                        <a class="nav-link" href="login.php">
                             <i class="mdi mdi-power"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item nav-settings d-none d-lg-block">
-                        <a class="nav-link" href="#">
-                            <i class="mdi mdi-format-line-spacing"></i>
                         </a>
                     </li>
                 </ul>
                 <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
-                    <span class="mdi mdi-menu"></span>
-                </button>
+            <span class="mdi mdi-menu"></span>
+          </button>
             </div>
         </nav>
         <!-- partial -->
@@ -177,20 +81,6 @@
             <!-- partial:partials/_sidebar.html -->
             <nav class="sidebar sidebar-offcanvas" id="sidebar">
                 <ul class="nav">
-                    <li class="nav-item nav-profile">
-                        <a href="#" class="nav-link">
-                            <div class="nav-profile-image">
-                                <img src="../assets/images/faces/face1.jpg" alt="profile">
-                                <span class="login-status online"></span>
-                                <!--change to offline or busy as needed-->
-                            </div>
-                            <div class="nav-profile-text d-flex flex-column">
-                                <span class="font-weight-bold mb-2">David Grey. H</span>
-                                <span class="text-secondary text-small">Project Manager</span>
-                            </div>
-                            <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-                        </a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="index.php">
                             <span class="menu-title">Dashboard</span>
@@ -211,11 +101,25 @@
                             </ul>
                         </div>
                     </li>
-                    <li class="nav-item">
+					<li class="nav-item">
                         <a class="nav-link" href="javascript:void(0);">
-                            <span class="menu-title">Inbox</span>
-                            <i class="mdi mdi-email-outline menu-icon"></i>
+                            <span class="menu-title">Manage Customer</span>
+                            <i class="mdi mdi-settings menu-icon"></i>
                         </a>
+                    </li>
+					<li class="nav-item">
+                        <a class="nav-link" data-toggle="collapse" href="#project" aria-expanded="false" aria-controls="project">
+                            <span class="menu-title">Manage Project</span>
+                            <i class="menu-arrow"></i>
+                            <i class="mdi mdi-wrench menu-icon"></i>
+                        </a>
+                        <div class="collapse" id="project">
+                            <ul class="nav flex-column sub-menu">
+                                <li class="nav-item"> <a class="nav-link" href="add-project.php">Add/View Project</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="add-sub-project.php">Add/View Sub-Project</a></li>
+                                <li class="nav-item"> <a class="nav-link" href="add-plot.php">Add/View Plot</a></li>
+                            </ul>
+                        </div>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="team.php">
@@ -224,67 +128,13 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#joining-link" aria-expanded="false" aria-controls="joining-link">
-                            <span class="menu-title">New Joining Link</span>
-                            <i class="menu-arrow"></i>
-                            <i class="mdi mdi-link menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="joining-link">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Right link</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Left Link</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#wallet" aria-expanded="false" aria-controls="wallet">
+                        <a class="nav-link" href="wallet.php">
                             <span class="menu-title">Wallet</span>
-                            <i class="menu-arrow"></i>
                             <i class="mdi mdi-wallet menu-icon"></i>
                         </a>
-                        <div class="collapse" id="wallet">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Total Earning</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Referal Income</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Matching Income</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Advanced Payment</a></li>
-                            </ul>
-                        </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#business-zone" aria-expanded="false" aria-controls="business-zone">
-                            <span class="menu-title">Business Zone</span>
-                            <i class="menu-arrow"></i>
-                            <i class="mdi mdi-cards-variant menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="business-zone">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Customer List</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#rankers-details" aria-expanded="false" aria-controls="rankers-details">
-                            <span class="menu-title">Rankers Details</span>
-                            <i class="menu-arrow"></i>
-                            <i class="mdi mdi-account-card-details menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="rankers-details">
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">All Rankers</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Left Rankers</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="javascript:void(0);">Right Rankers</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0);">
-                            <span class="menu-title">Terms & Conditions</span>
-                            <i class="mdi mdi-clipboard-text menu-icon"></i>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="javascript:void(0);">
+                        <a class="nav-link" href="login.php">
                             <span class="menu-title">Logout</span>
                             <i class="mdi mdi-logout menu-icon"></i>
                         </a>

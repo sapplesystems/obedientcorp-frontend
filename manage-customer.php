7a06d9215fff5b356203461626f4a5dc56f1b452
= <=>
2020-03-16 19:37:52 +0530

    update cusomer module

--- manage-customer.php
+++ manage-customer.php
@@ -13,6 +13,12 @@
         </div>
         <div class="row">
             <div class="col-12">
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Select Agent :</label>
+                    <div class="col-sm-4">
+                        <select class="form-control required" id="agent_listing" name="agent_listing"></select>
+                    </div>
+                </div>
                 <div class="card">
                     <div class="card-body p-3">
                         <h4 class="card-title mb-4">Edit/View Customer</h4>
@@ -20,85 +26,16 @@
                             <table class="table table-bordered custom_action" id="order-listing">
                                 <thead>
                                     <tr>
-                                        <th> Sr No. </th>
-                                        <th> Project Name </th>
-                                        <th> Unit Price </th>
-                                        <th> Desc </th>
-                                        <th> Action </th>
+                                        <th>Customer Name</th>
+                                        <th>Sex</th>
+                                        <th>Mobile</th>
+                                        <th>Address</th>
+                                        <th>Age</th>
+                                        <th>Email</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <td> 1 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 2 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 3 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 4 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 5 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 6 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 7 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 8 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 9 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                    <tr>
-                                        <td> 10 </td>
-                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
-                                        <td> 12600 </td>
-                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
-                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
-                                    </tr>
-                                </tbody>
+                                <tbody id="customers_list"></tbody>
                             </table>
                         </div>
                     </div>
@@ -109,3 +46,4 @@
     <!-- content-wrapper ends -->
     <!-- partial:partials/_footer.html -->
     <?php include_once 'footer.php'; ?>
+    <script src="assets/javascript/add_customer.js"></script>

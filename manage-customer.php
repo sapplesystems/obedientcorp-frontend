b2427add51b0b7c87a49c98364ef56e4a17bf2be
mansijsapple <mansij.srivastava@sapple.co.in>
2020-03-16 12:18:05 +0530

    added new file

---
+++ manage-customer.php
@@ -0,0 +1,111 @@
+<?php include_once 'header.php'; ?>
+<!-- partial -->
+<div class="main-panel ">
+    <div class="content-wrapper ">
+        <div class="row grid-margin">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <a href="javascript:void(0);" class="btn btn-gradient-primary">Add Customer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body p-3">
+                        <h4 class="card-title mb-4">Edit/View Customer</h4>
+                        <div class="overflowAuto">
+                            <table class="table table-bordered custom_action" id="order-listing">
+                                <thead>
+                                    <tr>
+                                        <th> Sr No. </th>
+                                        <th> Project Name </th>
+                                        <th> Unit Price </th>
+                                        <th> Desc </th>
+                                        <th> Action </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td> 1 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 2 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 3 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 4 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 5 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 6 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 7 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 8 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 9 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                    <tr>
+                                        <td> 10 </td>
+                                        <td> OBEDIENT CORPORATION GREEN CITY </td>
+                                        <td> 12600 </td>
+                                        <td> lorem Ipsum is simply dummy text of the printing and typesetting industry. </td>
+                                        <td> <i class="mdi mdi-pencil text-info"></i> &nbsp <i class="mdi mdi-delete text-danger"></i> </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- content-wrapper ends -->
+    <!-- partial:partials/_footer.html -->
+    <?php include_once 'footer.php'; ?>

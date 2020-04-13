1ce05af9b96885d92865de46ea7854d54d197526
= <=>
2020-04-13 11:46:22 +0530

    add two pages

---
+++ failure.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+if (isset($_SESSION['login_resp']['id']) && !empty($_SESSION['login_resp']['id'])) {
+    echo '<script type="text/javascript">window.location.href = "dashboard";</script>';
+    exit;
+}
+include_once 'common_html.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Obedient</title>
+        <!-- plugins:css -->
+        <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+        <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+        <!-- endinject -->
+        <!-- Plugin css for this page -->
+        <!-- End plugin css for this page -->
+        <!-- inject:css -->
+        <!-- endinject -->
+        <!-- Layout styles -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!-- End layout styles -->
+        <link rel="shortcut icon" href="assets/images/favicon.png" />
+    </head>
+
+    <body>
+        <!--<div id="loader_bg">
+            <div class="flip-square-loader mx-auto" id="loader_div"></div>
+        </div>-->
+        <div class="container-scroller">
+            <div class="container-fluid page-body-wrapper full-page-wrapper">
+                <div class="content-wrapper d-flex align-items-stretch auth auth-img-bg">
+                    <div class="row flex-grow">
+                        <div class="col-12 d-flex align-items-center justify-content-center">
+                            <div class="card">
+                                <div class="card-body p-3">
+                                    <div class="thank-div">
+                                        <div class="text-center"><i class="mdi mdi-close-circle"></i></div>
+                                        <h2 class="text-center">FAILURE</h2>
+                                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer elementum varius orci non iaculis.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- content-wrapper ends -->
+            </div>
+            <!-- page-body-wrapper ends -->
+        </div>
+        <!-- container-scroller -->
+        <!-- plugins:js -->
+        <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+        <!-- endinject -->
+        <!-- Plugin js for this page -->
+        <!-- End plugin js for this page -->
+        <!-- inject:js -->
+        <script src="assets/js/off-canvas.js"></script>
+        <script src="assets/js/hoverable-collapse.js"></script>
+        <script src="assets/js/misc.js"></script>
+        <script src="assets/js/settings.js"></script>
+        <script src="assets/js/todolist.js"></script>
+        <!-- endinject -->
+        <script src="assets/vendors/sweetalert/sweetalert.min.js "></script>
+        <script src="assets/js/alerts.js "></script>
+        <!--Validate jquery-->
+        <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+    </body>
+
+</html>
+<script>
+    var base_url = 'http://demos.sappleserve.com/obedient_api/public/api/';
+</script>

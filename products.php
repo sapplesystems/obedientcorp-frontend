8f50663bfbc317f33813f92f616276195d66c4fb
= <=>
2020-05-04 13:51:58 +0530

    update obedient_stage db, change in css/style/look-n-feel by afshan in style.css, products.php and collect-emi.php file

--- products.php
+++ products.php
@@ -3,10 +3,7 @@ include_once 'common_html.php';
 include_once 'header_frontend.php';
 ?>
 
-
-
 <?php echo $common['main_container_navigation']; ?>
-
 
 <!-- Dotted Navigation -->
 <?php echo $common['dotted_navigation']; ?>
@@ -14,7 +11,7 @@ include_once 'header_frontend.php';
 <!-- Page Title -->
 <section id="home" class="xl-py t-center white fullwidth">
     <!-- Background image - you can choose parallax ratio and offset -->
-    <div class="bg-parallax skrollr" data-anchor-target="#home" data-0="transform:translate3d(0, 0px, 0px);" data-900="transform:translate3d(0px, 200px, 0px);" data-background="images/product_bg.jpg" style="background-position: center 90%;"></div>
+    <div class="bg-parallax skrollr" data-anchor-target="#home" data-0="transform:translate3d(0, 0px, 0px);" data-900="transform:translate3d(0px, 200px, 0px);" data-background="images/product-banner.jpg" style="background-position: center 19%;"></div>
 </section>
 <!-- End Page Title --> 
 <!-- GALLERY -->
@@ -39,7 +36,7 @@ include_once 'header_frontend.php';
             <ul class="product_list" id="product_list"></ul>
         </div>
     </div>
-    <div class="clearfix"></div>
+    <div class="clearfix"></div> 
 </section>
 <div class="clearfix"></div>
 <!-- END GALLERY -->
@@ -47,21 +44,19 @@ include_once 'header_frontend.php';
 <?php include_once 'footer_frontend.php'; ?>
 <!-- END FOOTER -->
 
+
 <!-- SEARCH FORM FOR NAV -->
 <?php echo $common['search_form']; ?>
 
 <script type='text/javascript' src='js/jquery.js'></script>
+
 <!-- jQuery -->
 <script src="js/jquery.min.js?v=2.3"></script>
 <!-- MAIN SCRIPTS - Classic scripts for all theme -->
 <script src="js/scripts.js?v=2.3.1"></script>
 <!-- END JS FILES -->
 <script src="assets/javascript/our_products.js"></script>
-
-
 <script type='text/javascript' src='js/site.js'></script>
-
-
 
 </body>
 <!-- Body End -->

41bc8a251e5a36b57c223bf8950af94668977d59
= <=>
2020-06-08 18:50:53 +0530

    update config url

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$base_url = 'http://demos.sappleserve.com/obedient_api/public/api/';
-$media_url = 'http://demos.sappleserve.com/obedient_api/public/uploads/';
+$base_url = 'http://zxcvnm.myobedient.com/obedient_api/public/api/';
+$media_url = 'http://zxcvnm.myobedient.com/obedient_api/public/uploads/';
 $home_url = 'http://localhost/obedientcorp-frontend/';
 ?>
